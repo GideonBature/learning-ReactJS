6c225742d110c4735baa92ccb73713028d6dd7d1
GideonBature <infoaboutgideon@gmail.com>
2024-01-24 04:33:37 +0100

    add contents

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
 import { CORE_CONCEPTS } from './data.js';
 import './components/Header/Header.css'
 import './components/CoreConcepts/CoreConcepts.css'
 import TabButton from './components/TabButton/TabButton.jsx';
+import React from 'react';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -26,12 +34,23 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          {!selectedTopic && <p>Please select a topic</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({children}) {
+export default function TabButton({ children, onSelect }) {
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     )
 }
