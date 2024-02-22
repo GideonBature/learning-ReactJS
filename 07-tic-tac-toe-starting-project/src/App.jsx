baaa8457489184eadd6d5d7477a0c54c75a48b60
GideonBature <infoaboutgideon@gmail.com>
2024-02-22 15:32:07 +0100

    base files for tic-tac-toe game

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,33 @@
-
+import React from 'react'
+import Player from './components/Player''./components/Player.jsx' 
 function App() {
   
 
   return (
-    <>
-    </>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
   )
 }
 
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,13 @@
+const Player = ({name, symbol}) => {
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+}
+
+export default Player;
